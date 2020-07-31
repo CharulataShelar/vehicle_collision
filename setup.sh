bb120cbdc40c6c182e05db72ced83df9b6f09fca
Charulata Shelar <33387365+CharulataShelar@users.noreply.github.com>
2020-07-31 20:49:35 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"charulatashelar@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
